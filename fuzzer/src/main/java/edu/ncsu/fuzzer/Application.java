6ba78254c379650d9a1282283845eab085180b6d
kshittiz kumar <kkumar4@ncsu.edu>
2019-03-10 15:06:00 -0400

    Created branch fuzzer using fuzzing tool and checkout to that

--- fuzzer/src/main/java/edu/ncsu/fuzzer/Application.java
+++ fuzzer/src/main/java/edu/ncsu/fuzzer/Application.java
@@ -1,9 +1,23 @@
 package edu.ncsu.fuzzer;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello fuzzer");
+        try {
+        	Path currentRelativePath = Paths.get("").toAbsolutePath().getParent();
+        	String repoURL = currentRelativePath.toString() + "/iTrust2-v4";
+        	//System.out.println("Current relative path is: " + repoURL);
+			HandleGit git = new HandleGit(repoURL);
+		} catch (IOException | GitAPIException e) {
+			e.printStackTrace();
+		}
     }
 
 }
--- fuzzer/src/main/java/edu/ncsu/fuzzer/HandleGit.java
+++ fuzzer/src/main/java/edu/ncsu/fuzzer/HandleGit.java
@@ -2,28 +2,83 @@ package edu.ncsu.fuzzer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand.ListMode;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRefNameException;
+import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
+import org.eclipse.jgit.lib.Ref;
 
 public class HandleGit {
 	private Git repository = null;
-	public HandleGit(Git repository) {
+	private final static String BRANCHHEAD = "refs/heads/fuzzer";
+	private final static String BRANCH = "fuzzer";
+
+	public HandleGit(String repositoryURL) throws IOException, GitAPIException {
+
+		Git repository = Git.open(new File(repositoryURL));
 		this.repository = repository;
+		boolean isPresent = false;
+		// List the existing branches
+		List<Ref> listRefsBranches = repository.branchList().setListMode(ListMode.ALL).call();
+
+		for (Ref refBranch : listRefsBranches) {
+			if (BRANCHHEAD.equals(refBranch.getName())) {
+				isPresent = true;
+				break;
+			}
+		}
+
+		if (isPresent)
+			checkoutBranch(BRANCH);
+		else {
+			createBranch(BRANCH);
+			checkoutBranch(BRANCH);
+		}
+
 	}
-	
+
 	public void setGitRepository(Git repository) {
 		this.repository = repository;
 	}
-	
-	public void addFileToIndex(File fileToAdd, String gitLocalRepositoryPath) throws IOException, GitAPIException {
-	    String filePath = fileToAdd.getAbsolutePath().replace("\\","/").substring(gitLocalRepositoryPath.length()+1);
-	    repository.add().addFilepattern(filePath).call();
+
+	public void createBranch(String branchName) {
+		// Create a new branch
+		try {
+			repository.branchCreate().setName(branchName).call();
+
+		} catch (RefAlreadyExistsException e) {
+			e.printStackTrace();
+		} catch (RefNotFoundException e) {
+			e.printStackTrace();
+		} catch (InvalidRefNameException e) {
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public void checkoutBranch(String branchName) {
+		// Checkout the new branch
+		try {
+			repository.checkout().setName(branchName).call();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addFileToIndex(File fileToAdd, String gitLocalRepositoryPath) throws IOException, GitAPIException {
+		String filePath = fileToAdd.getAbsolutePath().replace("\\", "/").substring(gitLocalRepositoryPath.length() + 1);
+		repository.add().addFilepattern(filePath).call();
+	}
+
 	public void commitChanges(String commitMessage) throws GitAPIException {
-	    CommitCommand commit = repository.commit();
-	    commit.setAllowEmpty(false);
-	    commit.setMessage(commitMessage).call();
+		CommitCommand commit = repository.commit();
+		commit.setAllowEmpty(false);
+		commit.setMessage(commitMessage).call();
 	}
 }
