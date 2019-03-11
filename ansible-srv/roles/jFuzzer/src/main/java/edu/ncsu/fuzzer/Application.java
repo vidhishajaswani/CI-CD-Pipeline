296543b29490035cb7a3bdb4f883f6706f4223f4
Shivam Chamoli <schamol@ncsu.edu>
2019-03-11 17:04:31 -0400

    fuzzer java

---
+++ ansible-srv/roles/jFuzzer/src/main/java/edu/ncsu/fuzzer/Application.java
@@ -0,0 +1,25 @@
+package edu.ncsu.fuzzer;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Fuzzing Started!");
+		try {
+			Path currentRelativePath = Paths.get("").toAbsolutePath().getParent();
+			String repoURL = currentRelativePath.toString() + "/iTrust2-v4";
+			// System.out.println("Current relative path is: " + repoURL);
+			HandleGit git = new HandleGit(repoURL);
+			ItrustFuzzing fuzzing = new ItrustFuzzing(repoURL, git);
+			fuzzing.doFuzzing();
+		} catch (IOException | GitAPIException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
