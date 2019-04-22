a1a34d4851ca6bc1d853510a711937141a80693b
foo <bar@example.com>
2019-04-22 19:22:19 -0400

    itrust draft

--- ansible-srv/roles/aws/templates/main.py
+++ ansible-srv/roles/aws/templates/main.py
@@ -54,7 +54,7 @@ sec_group=sec.authorize_ingress(
 
 # Finally create instance by giving the Image ID and Instance Type
 instances = ec2.create_instances(
-    ImageId='ami-0565af6e282977273', InstanceType='t2.micro', MaxCount=1, MinCount=1,KeyName=awskey,
+    ImageId='ami-0565af6e282977273', InstanceType='t2.medium', MaxCount=1, MinCount=1,KeyName=awskey,
     NetworkInterfaces=[{'SubnetId': subnet.id, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True, 'Groups': [sec.group_id]}])
 
 #print(instances)
