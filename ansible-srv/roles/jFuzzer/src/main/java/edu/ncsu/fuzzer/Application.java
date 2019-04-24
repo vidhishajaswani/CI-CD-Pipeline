76aed59b5a264ec327b2dab736059f08204f99ad
kshittiz kumar <kkumar4@ncsu.edu>
2019-04-24 17:28:48 -0400

    fixed bug & commented

--- ansible-srv/roles/jFuzzer/src/main/java/edu/ncsu/fuzzer/Application.java
+++ ansible-srv/roles/jFuzzer/src/main/java/edu/ncsu/fuzzer/Application.java
@@ -26,6 +26,7 @@ public class Application {
 		} finally {
 			git.lapse(45000);
 			git.reset(); // reset back to original commit
+			System.out.println("Finally: Resetting to master");
 			git.checkoutBranch("master");
 		}
 	}
