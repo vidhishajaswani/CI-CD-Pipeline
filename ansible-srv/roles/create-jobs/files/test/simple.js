934b94e9372c273024f6326dbffa331f30f1f032
kshittiz kumar <kkumar4@ncsu.edu>
2019-04-27 19:28:08 -0400

    tried api/design/surveys, bad gateway issue'

--- ansible-srv/roles/create-jobs/files/test/simple.js
+++ ansible-srv/roles/create-jobs/files/test/simple.js
@@ -3,11 +3,12 @@ const expect = require('chai').expect;
 const main = require('../main')
 const got   = require('got');
 var http = require('http');
+var request = require('request');
 const execSync = require('child_process').execSync;
 const awsEc2Ip = execSync('npm config get awsEc2Ip',
     { stdio: ['ignore', 'pipe', 'pipe'] }).toString().replace(/\n$/, '');
-var checkbox_server_url = 'http://' + awsEc2Ip + '/'
-var end_point = checkbox_server_url + 'studies.html'
+var checkbox_server_url = 'http://' + awsEc2Ip;
+var end_point = checkbox_server_url + '/studies.html'
 
 describe('Array', function() {
   describe('#indexOf()', function() {
@@ -37,6 +38,25 @@ describe('main', function() {
 				  done();
 				  });
 				});
+				
+			  it('should properly create survey question', async () => {
+					  const response = await got.post(checkbox_server_url + '/api/design/survey', {
+						body: {
+					"markdown": "{NumberQuestions:true}\n-----------\n### Multiple Choice Question (Check all that apply)\nA *description* for question.\nQuestions are created with headers (level 3).\n* Choice A\n* Choice B\n* Choice C"
+				},
+						json: true,
+						timeout: 500
+					  });
+
+					  expect(response.body.preview).to.include('Multiple Choice Question');
+				});
+			 it('listing all surveys', function(done){
+					request(checkbox_server_url + '/api/design/survey/all/listing', function(error, response, body) {
+						expect(response.statusCode).to.equal(200);
+						done();
+					});
+				});
+
             });
 			
           // Stop server
