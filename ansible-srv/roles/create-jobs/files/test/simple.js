78fa19bd847af059aac5f3ef35df5ace62a4ca0f
kshittiz kumar <kkumar4@ncsu.edu>
2019-04-27 16:59:18 -0400

    removed comment

--- ansible-srv/roles/create-jobs/files/test/simple.js
+++ ansible-srv/roles/create-jobs/files/test/simple.js
@@ -8,8 +8,6 @@ const awsEc2Ip = execSync('npm config get awsEc2Ip',
     { stdio: ['ignore', 'pipe', 'pipe'] }).toString().replace(/\n$/, '');
 var checkbox_server_url = 'http://' + awsEc2Ip + '/'
 var end_point = checkbox_server_url + 'studies.html'
-console.log(checkbox_server_url)
-console.log(end_point)
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function(){
